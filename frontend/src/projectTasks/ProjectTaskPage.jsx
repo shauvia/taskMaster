e1facbc34a4e85318cee9ffbdd0b9f4d8ed1de3b
Ewa Szalwinska <e.szalwinska@gmail.com>
2026-05-19 15:39:28 -0700

    Fix case-sensitive import for ProjectTaskList

--- frontend/src/projectTasks/ProjectTaskPage.jsx
+++ frontend/src/projectTasks/ProjectTaskPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ProjTaskList } from "./ProjectTasklist.jsx";
+import { ProjTaskList } from "./ProjectTaskList.jsx";
 import { Link } from "react-router";
 import { getAllProjectTasks } from "../api/apiProjects.js";
 
